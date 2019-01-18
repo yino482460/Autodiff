771e474e65af9d6847c9b85fb87882456876d8d1
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-01-18 23:42:37 +0900

    ノードを作成関数を追加

--- Parse.c
+++ Parse.c
@@ -3,19 +3,51 @@
 #include <stdlib.h>
 #include <string.h>
 
+/***  Node 2019/01/18 23:30 ***/
+/* ノードのラベル */
+enum {
+    ND_NUM = 256,   //整数の場合におけるノードの型
+};
+/* ノード本体の設定 */
+typedef struct Node {
+    /* data */
+    int Nd_Ty;           // 演算子かND_NUM つまり 考えている対象の型
+    struct Node *Lhs;   // 左辺
+    struct Node *Rhs;   // 右辺
+    int Nd_Val;          // Nd_Tyが数字の場合のみ使う
+}Node;
+
+/* 新規ノードを作成する関数 (全てのノードに共通して生成する)*/
+Node *NewNode (int Nd_Ty, Node *Lhs, Node *Rhs){
+    Node *node = malloc(sizeof(Node));  //Node分のメモリを確保
+    node -> Nd_Ty = Nd_Ty;  //以下新規ノードに各値をセット
+    node -> Lhs = Lhs;
+    node -> Rhs = Rhs;
+    return node;
+}
+/* 新規に作成したノードに数字が含まれているときにNd_Valを追加する関数 */
+Node *NewNodeNum(int Nd_Val) {
+    Node *node = malloc(sizeof(Node));
+    node -> Nd_Ty = ND_NUM;
+    node -> Nd_Val = Nd_Val;
+    return node;
+}
+
+
+/***  Token 2019/01/18 10:00 ***/
 /* トークンが数値が四則演算記号、関数かを分類しラベルを貼る */
 enum {
     TK_NUM = 256,    /* 整数トークン */
-    TK_EOF,     /* トークン文字列の終わりを表す */
-
+    TK_EOF = -1,     /* トークン文字列の終わりを表す */
 };
 /* トークン(Token)の型の設定 */
 typedef struct {
     /* data */
     int TY; /* トークンの型 に対するラベル*/
-    int VAL;    /* TYがTK_NUMのときその数値を代入 */
+    int VAL;    /* TYが数字のときその数値を代入 */
     char *input;    /* トークン文字列(エラーメッセージ用) */
 }Token ;
+
 // トークナイズした結果のトークン列はこの配列に保存する
 // 100個以上のトークンは来ないものとする
 Token tokens[100];
@@ -57,27 +89,70 @@ void tokenize(char *p) {
 
 // エラーを報告するための関数
 void error(int i) {
-  fprintf(stderr, "予期しないトークンです: %s\n",
-          tokens[i].input);
+  fprintf(stderr, "予期しないトークンです: %s\n", tokens[i].input);
   exit(1);
 }
+
 
 //メイン関数
 int main(int argc, char const *argv[]) {
     FILE *file;
     char fname[] = "test.txt";  //読み込むファイルの名前
     int N = 100;    //一行当たりに読み込める最大文字数
-    char str[N];    //読み込んだ文字列
+    char formula[N];    //読み込んだ数式 = Numerical formula
 
+    //ファイルからの読み込み
     file = fopen(fname, "r");
+    //例外処理
     if ( file == NULL) {
         printf("%s file not open!\n", fname );
         return -1;
     }
-    while(fgets(str, N, file) != NULL) {
-		printf("%s", str);
-	}
+    /*一行丸ごと数式を読み込む　※複数の数式を読み込むならwhile文に　*/
+    fgets(formula, N, file);    //ファイルから読み込んでformulaに代入
     fclose(file);
-    
+    //読み込んだかの確認
+    //printf("%s", formula);
+
+    //ファイルから読み込んだ文字列をトークナイズする
+    tokenize( formula );
+
+    //トークナイズされたかの確認
+    //printf("%s\n", test );
+
+
+    // 式の最初は数でなければならないので、それをチェックして
+    int i = 0;
+    if (tokens[0].TY != TK_NUM){  //例外処理
+        error(0); }
+    printf("%d\n", tokens[0].VAL);
+
+    i = 1;
+    while (tokens[i].TY != TK_EOF) {
+        // +の場合
+        if (tokens[i].TY == '+') {
+            i++;
+            //+が最初または最後のとき式が成立しない
+            if (tokens[i].TY != TK_NUM) {
+                error(i);
+            }
+            printf("%d\n", tokens[i].VAL);
+            i++;
+            continue;
+        }
+        // - の場合
+        if (tokens[i].TY == '-') {
+            i++;
+            if (tokens[i].TY != TK_NUM) {
+                error(i);
+            }
+            printf("%d\n", tokens[i].VAL);
+            i++;
+            continue;
+        }
+
+    error(i);
+  }
+
     return 0;
 }
