daff3371bbbda22ca0356afa443d23bc5592f983
yino48246@gmail.com <y_inoue@inoueyoutanoMacBook-ea.local>
2019-01-20 00:35:27 +0900

    Tokenトークンを自由長に改善

--- Token.c
+++ Token.c
@@ -6,31 +6,40 @@
 #include "Autodiff.h"
 #include "Token.h"
 
-Token tokens[100];
+static Token *add_token(Vector *v, int ty, char *input) {
+    Token *t = malloc(sizeof(Token));
+    t->ty = ty;
+    t->input = input;
+    vec_push(v, t);
+    return t;
+}
 
 //  2018/01/18 23:00 Vector をまだ理解できてない(泣)
 Vector *tokenize(char *p){
+    Vector *v = new_vec();  //関数未定義?　2018/01/18
     int i = 0;
     while (*p) {
+        //空白を読み飛ばす
+        if (isspace(*p)) {
+            p++;
+            continue;
+        }
         if (*p == '+' || *p == '-' || *p == '*' || *p == '/') {
-            tokens[i].ty = *p;
-            tokens[i].input = p;
+            add_token(v, *p, p);
             i++;
             p++;
             continue;
         }
         if (isdigit(*p)) {
-            tokens[i].ty = TK_NUM;
-            tokens[i].input = p;
-            tokens[i].val = strtol(p, &p, 10);
+            Token *t = add_token(v, TK_NUM, p);
+            t->val = strtol(p, &p, 10);
             i++;
             continue;
         }
         fprintf(stderr, "トークナイズできません: %s\n", p);
         exit(1);
     }
-    tokens[i].ty = TK_EOF;
-    tokens[i].input = p;
 
-
+    add_token(v, TK_EOF, p);
+    return v;
 };
